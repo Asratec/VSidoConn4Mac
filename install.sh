f239949c2025df43471dd6dab88f7193c47d0c02
Ma Eikei <yma@asratec.co.jp>
2015-04-23 13:49:36 +0900

    document

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+wget --no-check-certificate https://asratec.github.io/VSidoConn4Mac/Binary/v0.81/VSidoConn4Mac.tar.gz -O VSidoConn4Mac.tar.gz 
+mkdir -p ./VSidoConn4Mac
+tar -xzvf VSidoConn4Mac.tar.gz -C ./VSidoConn4Mac
+
+
+
