4daeb3daa539b4733b81c4ebea418540b7ca4bb3
Ma Eikei <yma@asratec.co.jp>
2015-11-06 21:36:00 +0900

    fix directory structure of binay v0.96

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #curl -k http://asratec.github.io/VSidoConn4Mac/install.sh | sh
-curl -k http://asratec.github.io/VSidoConn4Mac/Binary/v0.81/VSidoConn4Mac.tar.gz -o VSidoConn4Mac.tar.gz
+curl -k http://asratec.github.io/VSidoConn4Mac/Binary/v0.96/VSidoConn4Mac.tar.gz -o VSidoConn4Mac.tar.gz
 mkdir -p ./VSidoConn4Mac
 tar -xzvf VSidoConn4Mac.tar.gz -C ./VSidoConn4Mac
 
