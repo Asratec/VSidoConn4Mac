498d6e4455c085209e8a411a3ad14a5a2c9979b8
Ma Eikei <yma@asratec.co.jp>
2016-01-28 14:14:02 +0900

    binary for release

--- install.sh
+++ install.sh
@@ -3,6 +3,5 @@
 curl -k http://asratec.github.io/VSidoConn4Mac/Binary/v0.96/VSidoConn4Mac.tar.gz -o VSidoConn4Mac.tar.gz
 mkdir -p ./VSidoConn4Mac
 tar -xzvf VSidoConn4Mac.tar.gz -C ./VSidoConn4Mac
-
-
-
+cd ./VSidoConn4Mac && unzip SelectRobot.zip
+cd -
